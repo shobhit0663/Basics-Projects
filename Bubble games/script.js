661fae56829a1e3a2835d697764e020e6d2195fa
shobhit <shobhitgaidhane105@gmail.com>
2025-02-13 13:58:49 +0530

    initial commit

---
+++ Bubble games/script.js
@@ -0,0 +1,59 @@
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+
+function increaseScore(){
+    score += 10;
+    document.querySelector("#scoreval").textContent = score ;
+}
+
+function getNewHit(){
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+
+function makeBubble() {
+    var clutter = "";
+    for (var i = 0; i < 119; i++) {
+        var rn = Math.floor(Math.random() * 10)
+        clutter += `<div class="bubble">${rn}</div>`;
+    }
+    document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+function runTimer() {
+
+    var timerint = setInterval(function () {
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        }else{
+            clearInteraval(timerint);
+            document.querySelector("#pbtm").innerHTML = `<h1> GAME OVER <h1>`;
+        }
+    }, 1000)
+}
+
+document.querySelector("#pbtm")
+.addEventListener("click",function (dets) {
+
+    var clickedum = Number(dets.target.textContent);
+    if(clickedum === hitrn){
+        increaseScore(); 
+        makeBubble();
+        getNewHit();
+    }
+
+
+});
+
+
+
+
+
+runTimer();
+makeBubble();
+getNewHit();
+
+
+
